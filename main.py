a9deef9afaafe3326745e56592983f383ad65642
Zasalkin <103993441+Zasalkin@users.noreply.github.com>
2022-05-26 19:16:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-sides = sorted(sides, reverse=True)
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-print("Максимальная площадь треугольника", smax)
+import requests
+
+
+s_city = "Moscow,RU"
+appid = "233aed3d1f3cb32258c82dc4980f2956"
+
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Город:", s_city)
+print(" Скорость ветра:", data['wind']['speed'])
+print("Видимость:", data['visibility'])
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+
+data = res.json()
+print("Скорость вестра и видимость на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nСкорость ветра <",
+'{0}'.format(i['wind']['speed']), "> \r\nВидимость <",
+i['visibility'], ">")
+print("____________________________")
